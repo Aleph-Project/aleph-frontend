87f252fc472ccaf34bd170a677630d0021de293d
IvanaPedraza <ivanapedra234@gmail.com>
2025-05-16 21:15:15 -0500

    ajustes vista información canciones 🦝

--- app/info-songs/page.tsx
+++ app/info-songs/page.tsx
@@ -25,11 +25,48 @@ Después de aquí nos vamos pa-
 Que hace tiempo por ahí no se ve?
 ¿Será que ya me superó y le va bien?
 Mientras que yo, borracho, pienso
-...
-
+¿Qué estará haciendo mi ex
+Que hace tiempo por ahí no se ve?
+¿Será que ya me superó y le va bien?
+Mientras que yo, borracho, pienso
+¿Qué diablo estará haciendo?
+¿Estará jangueando o estará durmiendo?
+¿Estará fumando o estará bebiendo?
+¿Seguirá sola o está saliendo
+Con otro que no soy yo?, no soy yo
+Mami, ese no soy yo, no soy yo
+Aposté que te olvidaba y perdí 500
+Otra vez me ganaron los sentimiento'
+Los muchachos piensan que yo estoy contento
+Pero no, estoy muerto por dentro
+La disco está llena y a la vez vacía
+Porque no está la nena mía
+Con la que yo siempre me reía
+Con la que yo siempre me venía
+Con la que yo hablaba to' los días
+Y ahora no sé na, y ahora no sé na, ey
+¿Qué estará haciendo mi ex
+Que hace tiempo por ahí no se ve?
+¿Será que ya me superó y le va bien?
+Mientras que yo, borracho, pienso
+2019, un pestañeo y ahora estamo aquí
+2020, la última vez que yo fui feliz
+2022, la última vez que yo te vi
+La vida no me cumplió na de lo que le pedí
+No sé qué pasó, yo le pedí a Dios
+Pero él también me ghosteó
+El futuro me golpeó, en el pasado me dejó
+La felicidad se alejó
+Y me pregunto qué estarás haciendo
+Si en mí estás pensando
+O si la luna estás viendo
+Con otra persona conectando
+Y si de mí le estás hablando, eh-eh
+Espero aunque sea ser un buen recuerdo
+Ser un buen recuerdo
 Fuente: Musixmatch
-Compositores: Marco Daniel Borrero / Benito Antonio Martinez Ocasio / Álvaro Jerez Barranco / Scott H Dittrich / Martin Hardie Coogan / Benjamin Falik / Yuval Haim Chain / Roberto Jose Jr Rosado Torres / Aidan James Cullen / Willkar Jose Soto Figueroa
-Letra de EL CLúB © Universal Music Corp., Straight From The Art Music, Songs Of Universal Inc., Cody Pablito Publishing, Howl Of The Moon Music, Six Ten Worldwide Publishing, Grass Is Greener Publishing, Howard Hues Music, Rsm Publishing Llc, Creative Park Publishing.`,
+Compositores: Marco Daniel Borrero / Benito Antonio Martinez Ocasio / álvaro Jerez Barranco / Scott H Dittrich / Martin Hardie Coogan / Benjamin Falik / Yuval Haim Chain / Roberto Jose Jr Rosado Torres / Aidan James Cullen / Willkar Jose Soto Figueroa
+Letra de EL CLúB © Universal Music Corp., Straight From The Art Music, Songs Of Universal Inc., Cody Pablito Publishing, Howl Of The Moon Music, Six Ten Worldwide Publishing, Grass Is Greener Publishing, Howard Hues Music, Rsm Publishing Llc, Creative Park Publishing`,
   };
   return (
     <div className="flex flex-col h-screen bg-black text-white pt-16 overflow-hidden">
@@ -38,15 +75,15 @@ Letra de EL CLúB © Universal Music Corp., Straight From The Art Music, Songs O
                 <div className="flex-1 flex flex-col overflow-hidden text-white ">
                     {/*contenedor de toda la información con su titulo */}
                     <div className="relative flex-1 overflow-auto bg-gray-700/45 rounded-xl m-4 overflow-auto h-auto">
-                        <div className="grid grid-cols-[1fr_2fr] gap-4 m-6">
-                            <div className="flex items-center">
+                        <div className="grid grid-cols-[1fr_2fr] gap-4 m-7">
+                            <div className="flex place-items-end">
                                 <h1 className="text-left text-7xl font-extrabold ">{songInfo.title}</h1>
                             </div>
-                            <div className="flex items-center col-span-1">
+                            <div className="flex place-items-end col-span-1 ">
                                 <CirclePlus style={{ width: '25%', height: '25%' }} className=" text-purple-200 text-left " />
                             </div>
                         </div>
-                        <div className="m-6">
+                        <div className="m-7">
                             <p className="text-justify w-[60%] ">{songInfo.description}</p>
                         </div>
                     
