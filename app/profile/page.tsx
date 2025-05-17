52caa2e0237332293a5e34c731a3807eef1b8364
kellysolanomt <ksolanoc@unal.edu.co>
2025-05-17 01:52:31 -0500

    Avance en perfil de usuario

---
+++ app/profile/page.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import { Sidebar } from "@/components/music-player/sidebar"
+import { MainContent } from "@/components/profile/main-content"
+
+export default function Profile() {
+    return(
+        <div className="flex flex-col h-screen bg-black text-white pt-16 overflow-hidden">
+            <div className="flex flex-1 overflow-hidden">
+                <Sidebar />
+                <div className="flex-1 p-4">
+                    <MainContent />
+                </div>
+            </div>
+        </div>
+    )
+}
